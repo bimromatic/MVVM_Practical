dde0b0f04785977c90c1a5bd908391ee456c3422
bimromatic <953364885@qq.com>
2021-12-03 00:10:39 +0800

    this is first commit

---
+++ VersionPlugin/src/main/java/com/bimromatic/plugin/BuildConfig.kt
@@ -0,0 +1,20 @@
+package com.bimromatic.plugin
+
+/**
+ * author : bimromatic
+ * e-mail : xxx@xx
+ * time   : 2021/11/30
+ * desc   : 配置和 Build 相关的
+ * version: 1.0
+ */
+object BuildConfig {
+    val compileSdkVersion = 31
+    val buildToolsVersion = "29.0.3"
+    val minSdk = 21
+    val targetSdk = 31
+    val versionCode = 1
+    val versionName = "1.0.0"
+    val androidJUnitRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
+
+
---
+++ VersionPlugin/src/main/java/com/bimromatic/plugin/VersionPlugin.kt
@@ -0,0 +1,18 @@
+package com.bimromatic.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * author : bimromatic
+ * e-mail : xxx@xx
+ * time   : 2021/11/30
+ * desc   : 动态插件库
+ * version: 1.0
+ */
+class VersionPlugin : Plugin<Project> {
+
+    override fun apply(p0: Project) {
+
+    }
+}
